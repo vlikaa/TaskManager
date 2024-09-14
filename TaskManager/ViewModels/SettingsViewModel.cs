8dfb228212fc64eae5504fd5486bedb66eb7adab
vlikaa <aliveliyev123654@gmail.com>
2024-09-14 18:40:44 +0400

    added light theme & theme swicher

--- TaskManager/ViewModels/SettingsViewModel.cs
+++ TaskManager/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace TaskManager.ViewModels;
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.Input;
 
-public class SettingsViewModel : BaseViewModel
+namespace TaskManager.ViewModels;
+
+public partial class SettingsViewModel : BaseViewModel
 {
-	
+	[RelayCommand]
+	private void ChangeTheme(string theme)
+	{
+		var newTheme = new ResourceDictionary
+		{
+			Source = theme switch
+			{
+				"Light" => new Uri("pack://application:,,,/Themes/LightTheme.xaml"),
+				"Dark" => new Uri("pack://application:,,,/Themes/DarkTheme.xaml"),
+				_ => new Uri("pack://application:,,,/Themes/SystemTheme.xaml")
+			}
+		};
+
+		Application.Current.Resources.MergedDictionaries.Clear();
+		Application.Current.Resources.MergedDictionaries.Add(newTheme);
+	}
 }
