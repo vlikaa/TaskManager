2bec005522e9687730d974f7cd3d254a5b83dfde
vlikaa <aliveliyev123654@gmail.com>
2024-09-18 16:47:57 +0400

    fix navigation button margin/padding

--- TaskManager/ViewModels/SettingsViewModel.cs
+++ TaskManager/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace TaskManager.ViewModels;
 
+[INotifyPropertyChanged]
 public partial class SettingsViewModel : BaseViewModel
 {
 	[RelayCommand]
@@ -14,11 +16,30 @@ public partial class SettingsViewModel : BaseViewModel
 			{
 				"Light" => new Uri("pack://application:,,,/Themes/LightTheme.xaml"),
 				"Dark" => new Uri("pack://application:,,,/Themes/DarkTheme.xaml"),
-				_ => new Uri("pack://application:,,,/Themes/SystemTheme.xaml")
+				_ => new Uri("pack://application:,,,/Themes/LightTheme.xaml")
 			}
 		};
 
-		Application.Current.Resources.MergedDictionaries.Clear();
+		switch (theme)
+		{
+			case "Light":
+				newTheme.Source = new Uri("pack://application:,,,/Themes/LightTheme.xaml");
+				IsLightTheme = true;
+				IsDarkTheme = false;
+				break;
+			case "Dark":
+				newTheme.Source = new Uri("pack://application:,,,/Themes/DarkTheme.xaml");
+				IsDarkTheme = true;
+				IsLightTheme = false;
+				break;
+		}
+
+
+		Application.Current.Resources.MergedDictionaries.RemoveAt(1);
 		Application.Current.Resources.MergedDictionaries.Add(newTheme);
+
 	}
+	
+	[ObservableProperty] private bool _isDarkTheme = true;
+	[ObservableProperty] private bool _isLightTheme;
 }
